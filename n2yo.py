871fb0445bcbd5be6123c7d3199acdb51c6d66a3
Alsaaram <131556435+Alsaaram@users.noreply.github.com>
2023-12-27 18:46:29 -0500

    Initial commit

---
+++ n2yo.py
@@ -0,0 +1,33 @@
+import requests
+
+def satellite_info(satIDs, observer_lat, observer_lng, observer_alt, seconds, api_key):
+    base_url = "https://api.n2yo.com/rest/v1/satellite/positions/"
+
+    for satID in satIDs:
+        url = f"{base_url}{satID}/{observer_lat}/{observer_lng}/{observer_alt}/{seconds}/&apiKey={api_key}"
+        response = requests.get(url)
+        data = response.json()
+
+        if 'positions' in data:
+            position = data["positions"][0]
+            print(f"\nSatellite Name ({satID}):", data["info"]["satname"])
+            print("Latitude:", position["satlatitude"])
+            print("Longitude:", position["satlongitude"])
+            print("Azimuth:", position["azimuth"])
+            print("Elevation:", position["elevation"])
+            print("RA:", position["ra"])
+            print("DEC:", position["dec"])
+            print("TimeStamp:", position["timestamp"])
+        else:
+            print(f"\nError for Satellite ID {satID}:", data.get("Error", "Unknown Error"))
+
+
+satIDs = [25544, 25543]
+observer_lat = 40.7127492
+observer_lng = -74.0059945
+observer_alt = 0
+seconds = 1
+api_key = "L6UN7M-VLF578-ZFEC2R-56G4"
+
+satellite_info(satIDs, observer_lat, observer_lng, observer_alt, seconds, api_key)
+
